40be50b857b953ff879192d82b849e732013826b
tawsif1 <60055830+tawsif1@users.noreply.github.com>
2021-06-07 17:47:46 +0600

    changes to fit description

--- src/component/Ratings.js
+++ src/component/Ratings.js
@@ -4,16 +4,23 @@ import {FaStar} from"react-icons/fa"
 import "./ratings.css"
 export default function Ratings() {
     const [ratingValue, setratingValue] = useState(0);
+    //dummy posting url
+    const url = "facebook.com"
+    
    
     
     return (
-        <div>
+        <div className="wrapper">
             {[...Array(5)].map((ele,i) =>{
             const rating = i+1;
-           return( <label><input type="radio" value={rating} 
-           onClick={() =>{setratingValue(rating) ; axios.post("url", ratingValue)}}/>
-               <FaStar size={100} color={ ratingValue <  rating ? "#EECBF6":"#B11ACF" }
-               /></label> )})}
+           return( <div style={{display: 'inline-block'}} onMouseEnter={(e) => {
+            
+           setratingValue(rating)
+           }} onMouseLeave={()=> setratingValue(0)} onClick={axios.post(url, ratingValue)} >
+           <label><input type="radio" value={rating} 
+            />
+               <FaStar className="star" size={100} color={ ratingValue <  rating ? "#EECBF6":"#B11ACF" }
+               /></label> </div>)})}
                <h1>{ratingValue}</h1>
                
         </div>
